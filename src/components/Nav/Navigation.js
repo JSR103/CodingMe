59521572c5fd837d501a097fb25341386aea4089
Joel Silva Rivera <silva.joel.r@gmail.com>
2020-06-23 13:26:43 -0700

    Commit 3 1. Removed "4" from glitch 2. Updated to Nav with style and contact, about and home are placeholders for now

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -4,25 +4,64 @@ import { NavLink } from "react-router-dom";
 class Navigation extends React.Component {
   render() {
     return (
-      <nav>
-        <ul>
-          <li>
-            <NavLink exact activeClassName="current" to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink exact activeClassName="current" to="/about">
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink exact activeClassName="current" to="/contact">
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="Nav">
+        <nav>
+          <section className="buttons">
+            <div className="container">
+              <a
+                href="https://www.jsilvar.com"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="btn btn-1 "
+              >
+                <svg>
+                  <rect x="0" y="0" fill="none" width="100%" height="100%" />
+                </svg>
+                Jsilvar
+              </a>
+            </div>
+            <div className="container">
+              <NavLink
+                className="btn btn-1 "
+                exact
+                activeClassName="current"
+                to="/contact"
+              >
+                <svg>
+                  <rect x="0" y="0" fill="none" width="100%" height="100%" />
+                </svg>
+                Contact
+              </NavLink>
+            </div>
+            <div className="container">
+              <NavLink
+                className="btn btn-1 "
+                exact
+                activeClassName="current"
+                to="/about"
+              >
+                <svg>
+                  <rect x="0" y="0" fill="none" width="100%" height="100%" />
+                </svg>
+                About
+              </NavLink>
+            </div>
+            <div className="container">
+              <NavLink
+                className="btn btn-1 "
+                exact
+                activeClassName="current"
+                to="/"
+              >
+                <svg>
+                  <rect x="0" y="0" fill="none" width="100%" height="100%" />
+                </svg>
+                Home
+              </NavLink>
+            </div>
+          </section>
+        </nav>
+      </div>
     );
   }
 }
--- src/components/Views/Home.js
+++ src/components/Views/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => (
   <div className="home">
-    <h1 className="glitch4">WELCOME!</h1>
+    <h1 className="glitch">WELCOME!</h1>
     <p> Feel free to browse around and learn more about me.</p>
   </div>
 );
